7b1e2e47e07ef556fd97ed3111806b15b72ee1ee
Nguyen Chau Chieu <chauchieunguyen@gmail.com>
2020-04-22 01:12:35 +0700

    First commit

---
+++ MapleLabs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapleLabs
+//
+//  Created by Nguyen Chau Chieu on 4/22/20.
+//  Copyright © 2020 Nguyen Chau Chieu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
